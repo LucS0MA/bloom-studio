8f584ee8af3310d794d30e8be4c96cd6b39a05aa
LucS0MA <lucien.kohli@gmail.com>
2026-01-23 13:30:39 +0100

    smooth scroll & scroll to top after nav ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,29 @@
 import { Route, Routes } from "react-router";
+import ReactLenis from "lenis/react";
+
 import Layout from "./components/layout/layout";
 import Home from "./pages/home";
 import About from "./pages/about";
 import OnlineStudio from "./pages/onlineStudio";
 import Classes from "./pages/classes";
 import Contact from "./pages/contact";
+import ScrollToTop from "./components/ui/scrollToTop";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="contact" element={<Contact />} />
-        <Route path="classes" element={<Classes />} />
-        <Route path="online" element={<OnlineStudio />} />
-      </Route>
-    </Routes>
+    <ReactLenis root options={{ lerp: 0.1 }}>
+      <ScrollToTop />
+
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="classes" element={<Classes />} />
+          <Route path="online" element={<OnlineStudio />} />
+        </Route>
+      </Routes>
+    </ReactLenis>
   );
 }
 
